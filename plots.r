4512c8be2fa4213f6e494bf5f35aad6b923aff49
Ben Wendt <ben@theworkinggroup.ca>
2016-04-14 22:17:31 -0400

    Add legend

--- plots.r
+++ plots.r
@@ -4,3 +4,9 @@ plot(names[names$Name == 'Neil', c("Year","Count")], ylim=c(0,15000), type="l", 
 
 lines(names[names$Name =='Harold', c("Year","Count")], col="red")
 lines(names[names$Name =='Samuel', c("Year","Count")], col="green")
+
+legend(1960,10000,
+       c("Neil","Harold","Samuel"),
+       lty=c(1,1,1),
+       lwd=c(1,1,1),col=c("blue","red","green")
+      )
